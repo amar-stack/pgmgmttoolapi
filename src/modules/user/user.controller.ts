6c154a56b5c5a127ffa2d7090a25a62dd336777f
unknown <amarnad.v@gmail.om>
2024-03-28 16:53:13 +0530

    initial commit1

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,39 +7,6 @@ import pick from '../utils/pick';
 import { IOptions } from '../paginate/paginate';
 import * as userService from './user.service';
 
-// const accessKeyId = 'AKIASAZF4KVXGYXY5SMV';
-// const secretAccessKey = '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';
-// const region = 'Asia Pacific (Mumbai) ap-south-1';
-// const Bucket = 'pgmgmttool-bucket';
-
-// const s3 = new AWS.S3({
-//   region: BUCKET_REGION,
-//   accessKeyId: AWS_ACCESS_KEY_ID,
-//   secretAccessKey: AWS_SECRET_ACCESS_KEY,
-// });
-
-// function multipart(request: any) {
-//   // eslint-disable-next-line no-async-promise-executor
-//   return new Promise<void>(async (resolve, reject) => {
-//     const { headers } = request;
-//     const busboy = Busboy({ headers });
-//     // you may need to add cleanup logic using 'busboy.on' events
-//     busboy.on('error', (err: any) => reject(err));
-//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//     busboy.on('file', function (_fieldName: any, fileStream: any, fileName: any, _encoding: any, _mimeType: any) {
-//       const params = {
-//         Bucket: BUCKET_NAME,
-//         Key: fileName,
-//         Body: fileStream,
-//       };
-//       s3.upload(params)
-//         .promise()
-//         .then(() => resolve());
-//     });
-//     request.pipe(busboy);
-//   });
-// }
-
 export const createUser = catchAsync(async (req: Request, res: Response) => {
   const user = await userService.createUser(req.body);
   res.status(httpStatus.CREATED).send(user);
